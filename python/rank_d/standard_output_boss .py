d46aef4db4894d8cd8a7be56e929a88e653e8bc6
denyuyo <ph.c20h14o4@gmail.com>
2023-08-05 19:48:21 +0900

    numのあとに謎のprintして詰んでた

--- python/rank_d/standard_output_boss .py
+++ python/rank_d/standard_output_boss .py
@@ -7,3 +7,17 @@ for i in range(1, 1001):
 
 for i in range(1, 1001):
         print(i, end=' ' if i < 1000 else '\n')
+
+# ゼロ・プラス・マイナスを繰り返し判定する
+
+n = int(input())
+print(n)
+
+for i in range(n):
+        num = int(input())
+if num == 0:
+        print(f"{num}は0")
+elif num > 0:
+        print(f"{num}はプラス")
+else:
+        print(f"{num}はマイナス")
