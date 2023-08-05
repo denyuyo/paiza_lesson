5207d6ea0cfa9221629b93aa02340e1c44ab0794
denyuyo <ph.c20h14o4@gmail.com>
2023-08-05 18:16:11 +0900

    i %== 0であまりなく割り切れるかどうか判断してるの賢すぎないか？プログラム

--- python/rank_d/FizzBuzz.py
+++ python/rank_d/FizzBuzz.py
@@ -1,1 +1,18 @@
-# 
+# 1 ~ N の整数を 1 から順に改行区切りで出力
+
+n = int(input())
+
+for i in range(1, n+1):
+    print(i)
+    
+# FizzBuzz
+
+for i in range(1, 101):
+ if i % 3 == 0 and i % 5 == 0:
+     print("FizzBuzz")
+ elif i % 3 == 0:
+     print("Fizz")
+ elif i % 5 == 0:
+     print("Buzz")
+ else:
+     print(i)
