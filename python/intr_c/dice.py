a4b3a7be1731c132f21ee4e2577f54b5d21a7f59
denyuyo <ph.c20h14o4@gmail.com>
2023-08-16 21:11:35 +0900

    スライムの体重計算

--- python/intr_c/dice.py
+++ python/intr_c/dice.py
@@ -6,3 +6,11 @@ print("サイコロの目は" + str(dice) + "です。")
 
 enemy = random.randint(50,99)
 print("モンスターに、"+str(enemy)+"のダメージを与えた。")
+
+# スライムの合計体重を出力
+
+import random
+number = random.randint(1, 10)
+print("体重100キロのスライムが" + str(number) + "匹あらわれた")
+weight = 100
+print("スライムの合計体重は" + str(weight * number) + "キロです")
