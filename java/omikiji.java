08b1487893a5716b4326aa441e634882a89493f6
denyuyo <ph.c20h14o4@gmail.com>
2023-08-25 23:01:27 +0900

    count

--- java/omikiji.java
+++ java/omikiji.java
@@ -101,3 +101,14 @@ public class Main {
         System.out.println(number);
     }
 }
+
+// 肉の量り売り
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 128;
+        int weight = 300;
+        int amount = price * 3;
+        System.out.println("100グラム" + price + "円の肉、" + weight + "グラムは、" + amount + "円です。");
+    }
+}
