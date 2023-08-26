ad63cbb41c3c6e05619c60b99ddfbf925a74d287
denyuyo <ph.c20h14o4@gmail.com>
2023-08-26 21:48:00 +0900

    loop 10count

---
+++ java/loopback.java
@@ -0,0 +1,10 @@
+// whileによるループ処理
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;    // カウンタ変数の初期化
+        while (i <= 9) {    // 0　-> 1 -> 2 -> 3 ・・・ 5 -> 6
+            System.out.println("ハローpaizaラーニング");    // 繰り返し処理
+            i = i + 1;    // カウンタ変数の更新
+        }
+    }
+}
