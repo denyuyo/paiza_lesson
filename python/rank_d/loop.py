280aea0178d99de0c5d4da123ba2c9a0a0087853
denyuyo <ph.c20h14o4@gmail.com>
2023-09-06 08:09:48 +0900

    total value

--- python/rank_d/loop.py
+++ python/rank_d/loop.py
@@ -4,3 +4,13 @@ enemy = ["スライム", "モンスター", "ゾンビ", "ドラゴン", "魔王
 
 for i in enemy:
     print(i + "が現れた！")
+
+# 合計値
+
+numbers = [12, 34, 56, 78, 90]
+total = 0
+
+for num in numbers:
+    total += num
+
+print(total)
