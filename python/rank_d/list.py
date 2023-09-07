2c72fe3819407bd27cc4bea44da2ffde8ebacdeb
denyuyo <ph.c20h14o4@gmail.com>
2023-09-07 18:22:23 +0900

    English word count

--- python/rank_d/list.py
+++ python/rank_d/list.py
@@ -15,3 +15,10 @@ team.pop(0)
 
 team_str = "勇者,戦士,忍者,魔法使い"
 print(team_str.split(","))
+
+#英文の単語数を数える
+
+str = "One cold rainy day when my father was a little boy he met an old alley cat on his street"
+
+words = len(str.split())
+print(words)
