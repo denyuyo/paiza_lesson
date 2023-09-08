f4f5fc2de1947e22760559152859d985143ae8e4
denyuyo <ph.c20h14o4@gmail.com>
2023-09-08 18:50:17 +0900

    Output of multiple lines read

--- python/rank_d/list.py
+++ python/rank_d/list.py
@@ -28,3 +28,10 @@ print(words)
 
 url_str = input().rstrip()
 print(url_str.split("/"))
+
+# 読み込んだ複数行を出力する
+
+import sys
+for line in sys.stdin.readlines():
+	msg = line.rstrip()
+	print(msg + "が現れた")
