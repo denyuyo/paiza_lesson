d1c1006ef2721443711f3eb0d7555d2c9175fa0c
denyuyo <ph.c20h14o4@gmail.com>
2023-09-08 18:49:08 +0900

    Split loaded urls

--- python/rank_d/list.py
+++ python/rank_d/list.py
@@ -11,14 +11,20 @@ team.pop(0)
 
 
 
-#文字列をカンマで分割
+#文字列をカンマ分割
 
 team_str = "勇者,戦士,忍者,魔法使い"
 print(team_str.split(","))
 
-#英文の単語数を数える
+#英文の単語数
 
 str = "One cold rainy day when my father was a little boy he met an old alley cat on his street"
 
 words = len(str.split())
 print(words)
+
+# 標準入力から読み込んだURLを分割
+# https://paiza.jp/cgc/users/ready
+
+url_str = input().rstrip()
+print(url_str.split("/"))
