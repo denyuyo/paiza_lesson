616e1b3cccd624e93638dc822d9f7c414da65813
denyuyo <ph.c20h14o4@gmail.com>
2023-09-12 19:53:11 +0900

    Dictionary plus

--- python/rank_d/lexicon.py
+++ python/rank_d/lexicon.py
@@ -7,3 +7,15 @@ print(skills)
 level = "職業"
 print(skills[level])
 
+# 辞書要素追加
+
+skills = {"職業":"戦士", "体力":100, "魔法力":200, "ゴールド":380}
+# 長さ
+print(len(skills))
+# データ追加
+skills["属性"] = "炎"
+# 更新
+skills["職業"] = "魔法使い"
+# 削除
+del skills["体力"]
+print(skills)
