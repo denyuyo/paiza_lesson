1e19173ee7026c2543fdba2299a46fd49e444660
denyuyo <ph.c20h14o4@gmail.com>
2023-09-23 20:46:16 +0900

    Map display with 2D listings[2]

--- python/rank_d/Dimension_list.py
+++ python/rank_d/Dimension_list.py
@@ -150,3 +150,18 @@ for i,line in enumerate(landmap):
     for area in line:
         print(area, end="")
     print()
+
+# 2次元リストで地図を表示する[2]
+
+landmap = [["森" for i in range(20)] for j in range(10)]
+landmap[0][0] = "城"
+landmap[0][19] = "町"
+landmap[9][19] = "町"
+for i,line in enumerate(landmap):
+    print(str(i) + ":", end="")
+    for j,area in enumerate(line):
+        if (i % 2 == 0 or j % 3 == 0) and area == "森":
+            print("＋", end="")
+        else:
+            print(area, end="")
+    print()
