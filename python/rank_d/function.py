839e79ef7ea78b593e2a78f08faf969d4ecc6d18
denyuyo <ph.c20h14o4@gmail.com>
2023-09-26 23:24:21 +0900

    Add arguments & return values

--- python/rank_d/function.py
+++ python/rank_d/function.py
@@ -4,3 +4,13 @@ def say_hello():
     print("hello python")
 
 say_hello()
+
+# 引数と戻り値を追加する
+
+def sum(x, y):
+    return x + y
+    
+num1 = sum(555, 777)
+print(num1)
+num2 = sum(5555, 7777)
+print(num2)
