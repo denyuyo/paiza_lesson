87726ec6de40893eb6ba8e19e89b6107301b8294
denyuyo <ph.c20h14o4@gmail.com>
2023-10-02 23:25:12 +0900

    Manage variables with classes

--- python/rank_d/class.py
+++ python/rank_d/class.py
@@ -10,3 +10,21 @@ class Player:
 player1 = Player()
 player1.walk()
 player1.attack("スライム")
+
+# 変数をクラスで管理する
+
+class Player:
+    def __init__(self, job):
+        self.job = job
+    
+    def walk(self):
+        print(self.job + "は荒野を歩いていた。")
+
+player1 = Player("戦士")
+player1.walk()
+
+player2 = Player("魔法使い")
+player2.walk()
+
+player1.walk()
+
