963f6564cb3a519e4edf073928f6bf46e93025fb
denyuyo <ph.c20h14o4@gmail.com>
2023-10-04 23:43:16 +0900

    Tax calculation with tax

--- python/rank_d/class.py
+++ python/rank_d/class.py
@@ -45,3 +45,22 @@ enemies.append(Enemy("ドラゴン"))
 for enemy  in enemies:
     enemy.attack()
 
+# クラスで、引数と戻り値のあるメソッドを作る
+
+class Item:
+    tax = 1.08
+    
+    def __init__(self, price, quantity):
+        self.price = price
+        self.quantity = quantity
+    
+    def total(self):
+        return int(self.price * self.quantity * Item.tax)
+
+apple = Item(120, 15)
+total = apple.total()
+print("合計金額は" + str(total) + "円です")
+
+orange = Item(85, 32)
+total = orange.total()
+print("合計金額は" + str(total) + "円です")
