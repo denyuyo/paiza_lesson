d8b8bb93fe326d1a414ec1be899e92c22f3edf3a
denyuyo <ph.c20h14o4@gmail.com>
2023-10-09 22:26:37 +0900

    Method override

--- python/rank_d/class+.py
+++ python/rank_d/class+.py
@@ -15,3 +15,25 @@ box.open()
 jewelrybox = JewelryBox("魔法の指輪")
 jewelrybox.look()
 jewelrybox.open()
+
+# メソッドのオーバーライド
+class Box:
+    def __init__(self, item):
+        self.item = item
+
+    def open(self):
+        print("宝箱を開いた。" + self.item + "を手に入れた。")
+
+class MagicBox(Box):
+    def look(self):
+        print("宝箱は、妖しく輝いている。")
+    
+    def open(self):
+        print("宝箱を開いた。" + self.item + "が襲ってきた!")
+
+box = Box("大根")
+box.open()
+
+magicBox = MagicBox("料理人")
+magicBox.look()
+magicBox.open()
