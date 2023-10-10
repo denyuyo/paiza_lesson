6f1c14757e203a8870adbca41171024bc9d1cddd
denyuyo <ph.c20h14o4@gmail.com>
2023-10-10 23:30:24 +0900

    Describe an RPG player using inheritance

--- python/rank_d/class+.py
+++ python/rank_d/class+.py
@@ -37,3 +37,20 @@ box.open()
 magicBox = MagicBox("料理人")
 magicBox.look()
 magicBox.open()
+
+# RPGのプレイヤーを継承で記述１
+class Player:
+    def __init__(self, name):
+        self.name = name
+    
+    def attack(self, enemy):
+        print(self.name + "は、" + enemy + "を攻撃した！")
+    
+print("== パーティーでスライムと戦う ===")
+hero = Player("勇者")
+# hero.attack("スライム")
+warrior = Player("戦士")
+
+party = [hero, warrior]
+for member in party:
+    member.attack("スライム")
