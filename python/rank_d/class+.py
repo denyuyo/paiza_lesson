728c567aefb43f7d76f5ab95a5dee78d663d68c4
denyuyo <ph.c20h14o4@gmail.com>
2023-10-11 10:42:41 +0900

    Describe an RPG player using inheritance 2

--- python/rank_d/class+.py
+++ python/rank_d/class+.py
@@ -46,11 +46,34 @@ class Player:
     def attack(self, enemy):
         print(self.name + "は、" + enemy + "を攻撃した！")
     
-print("== パーティーでスライムと戦う ===")
+print("=== パーティーでスライムと戦う ===")
 hero = Player("勇者")
 # hero.attack("スライム")
 warrior = Player("戦士")
 
 party = [hero, warrior]
+for member in party:
+    member.attack("スライム")
+
+# RPGのプレイヤーを継承で記述２
+class Player:
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self, enemy):
+        print(self.name + "は、" + enemy + "を攻撃した！")
+
+class Wizard(Player):
+    def attack(self, enemy):
+        print("ズバーン！")
+        print(self.name + "は、" + enemy + "に炎を放った！")
+
+print("=== パーティーでスライムと戦う ===")
+hero = Player("勇者")
+# hero.attack("スライム")
+warrior = Player("戦士")
+wizard = Wizard("魔法使い")
+
+party = [hero, warrior, wizard]
 for member in party:
     member.attack("スライム")
