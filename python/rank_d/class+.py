db67f09f29635b7c550795a7fd0735d9dea20fe3
denyuyo <ph.c20h14o4@gmail.com>
2023-10-15 08:35:18 +0900

    Initially world standard time

--- python/rank_d/class+.py
+++ python/rank_d/class+.py
@@ -147,3 +147,18 @@ for member in party:
     member.attack("スライム")
 
 Player.summary()
+
+# 標準ライブラリを読み込んでみよう
+
+from datetime import datetime, timedelta, timezone
+
+jst = timezone(timedelta(hours=9))
+today = datetime.now(jst)
+print(today)
+print(today.year)
+print(today.month)
+print(today.day)
+
+day = datetime.strptime("2030/01/10 06:02:19", "%Y/%m/%d %H:%M:%S")
+print(day)
+
