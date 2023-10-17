bb2eb2a1d6398e2284eccea807daf2f1844a10dc
denyuyo <ph.c20h14o4@gmail.com>
2023-10-17 18:35:56 +0900

    Error message

--- python/rank_d/exception_handling.py
+++ python/rank_d/exception_handling.py
@@ -9,3 +9,17 @@ except ZeroDivisionError as e:
     print(e)
 finally:
     print(2)
+
+# いろいろな形式で例外に対応しよう
+
+print(1)
+try:
+    number = 0
+    answer = 100 / number
+    print(answer)
+except ZeroDivisionError as e:
+    print("0では割り算できません")
+    # print(traceback.format_exc())
+    sys.stderr.write(traceback.format_exc())
+finally:
+    print(2)
