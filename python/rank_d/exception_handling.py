fc43cceb18c6a40ee7be442bf0f079b3f1bc5175
denyuyo <ph.c20h14o4@gmail.com>
2023-10-18 19:35:08 +0900

    Name Error

--- python/rank_d/exception_handling.py
+++ python/rank_d/exception_handling.py
@@ -23,3 +23,16 @@ except ZeroDivisionError as e:
     sys.stderr.write(traceback.format_exc())
 finally:
     print(2)
+
+# 発生させる例外を変えてみよう
+
+print(1)
+try:
+    number = 1
+    answer = 100 / number
+    print(answer2)
+except NameError as e:
+    print("未定義の変数を呼び出しています")
+    print(e)
+finally:
+    print(2)
