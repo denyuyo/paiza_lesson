60d26514b976cfa93c9e5e124115036e7ca186f8
denyuyo <ph.c20h14o4@gmail.com>
2023-10-19 23:22:44 +0900

    Exception error is generic

--- python/rank_d/exception_handling.py
+++ python/rank_d/exception_handling.py
@@ -36,3 +36,22 @@ except NameError as e:
     print(e)
 finally:
     print(2)
+
+# 複数の例外を捕捉してみよう
+
+print(1)
+try:
+    number = 0
+    answer = 100 / number
+    print(answer2)
+except Exception as e:
+    print("予期せぬエラーが発生しました")
+    print(e)
+except ZeroDivisionError as e:
+    print("0では割り算できません")
+    print(e)
+except NameError as e:
+    print("未定義の変数を呼び出しています")
+    print(e)
+finally:
+    print(2)
