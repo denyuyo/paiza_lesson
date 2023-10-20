b8870f504d386d5b6765a628a50f78f030ca2cf3
denyuyo <ph.c20h14o4@gmail.com>
2023-10-20 23:10:48 +0900

    I'll pair RAISE with EXCEPTION

--- python/rank_d/exception_handling.py
+++ python/rank_d/exception_handling.py
@@ -55,3 +55,16 @@ except NameError as e:
     print(e)
 finally:
     print(2)
+
+# raiseで意図的に例外を投げよう
+
+print(1)
+try:
+    print(2)
+    raise Exception("意図的な例外")
+    print(3)
+except Exception as e:
+    print("予期せぬエラーが発生しました")
+    print(e)
+finally:
+    print(4)
